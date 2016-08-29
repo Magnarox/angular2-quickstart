e9f84716b57b7de543c00c000b50fc5fc41b4e80
Magnarox <thevinc@gmx.fr>
2016-08-29 16:19:31 +0200

    Angular2 QuickStart Complete

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by vidautre on 29/08/2016.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector : 'my-app',
+    template : '<h1>My First Angular 2 Application</h1>'
+})
+export class AppComponent {}
---
+++ app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by vidautre on 29/08/2016.
+ */
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+import {AppModule} from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
